29af9e38a5a3e21713e2ff8280505ddaeb58e988
BenceLevDev <bencelevdev@gmail.com>
2026-02-27 19:09:22 +0100

    Changed search-list API

--- frontend/src/components/InformationSidebar.jsx
+++ frontend/src/components/InformationSidebar.jsx
@@ -166,7 +166,7 @@ function InformationSidebar({
     const fetchMembers = async () => {
       try {
         const response = await fetch(
-          "http://127.0.0.1:8000/api/members/search-list",
+          "https://familytree-backend-9ua6.onrender.com/api/members/search-list",
           {
             headers: {
               Authorization: `Bearer ${token}`,
